cc839e7c9ebf75a07fe9447ad167cd8093faa23f
Austin Lovell <austin.d.lovell@gmail.com>
2017-02-17 20:08:14 -0700

    check point

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const app			= express();
 
-const recipe = require('../Recipe.json');
+const recipes = require('../Recipes.json');
+
+let db = require('../db.json');
 
 
 app.get('/', function (req, res) {
-  res.send(recipe)
+  res.send(recipes)
+})
+
+app.get('/recipes/:id', function (req, res) {
+  let recipeId = req.params.id;
+  let recipe = recipes[recipeId];
+
+  res.send(recipe);
 })
 
 app.get('/recipes/:id/*', function (req, res) {
-	const params = req.params[0].split("/")
+  let recipeId = req.params.id;
+	let path = req.params[0].split("/");
+  let recipe = recipes[recipeId];
 
-	// For loop over params and recipe
+  let validPath = validatePath(recipe, path);
+  let dbRes = getData(db, path);
 
-	console.log(params)
-  res.send(recipe)
+  res.send(dbRes);
 })
+
+
+function validatePath(recipe, paths, index = 0){
+  let key = paths[index];
+  let nextRecipe = recipe._type === "List" ? recipe._list : recipe[key];
+  let newIndex = index + 1;
+  return paths[newIndex] ? validatePath(nextRecipe, paths, newIndex) : nextRecipe;
+}
+
+function getData(db, paths, index = 0){
+  let key = paths[index];
+  let dbRes = db[key];
+  let newIndex = index + 1;
+  return paths[newIndex] ? getData(dbRes, paths, newIndex) : dbRes;
+}
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
